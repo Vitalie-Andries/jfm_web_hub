54f72c2b2e4a8cbed54f6b22d8cea8e01159d845
Vitalie Andries <35801489+Vitalie-Andries@users.noreply.github.com>
2022-04-13 19:27:28 +0300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6b57ec25c6cf60ec46a13b03ddb28213",
-"index.html": "dee86b39ef65dc33fbdd7fe703de964a",
-"/": "dee86b39ef65dc33fbdd7fe703de964a",
-"main.dart.js": "bc96f92e8a4db665b421a42f43976631",
+"index.html": "9caf64559b638561a568d4dd0b3fa171",
+"/": "9caf64559b638561a568d4dd0b3fa171",
+"main.dart.js": "b8ca4b49419a32de03e46ac2ee165f87",
 "favicon.png": "06f7ffb5e40e910067f51e2bcd3a9d95",
 "icons/Icon-192.png": "20efc95f17bf9893bed9be44e3b98bcf",
 "icons/Icon-maskable-192.png": "575eceea9ff624c4a5cf51d11c994678",
