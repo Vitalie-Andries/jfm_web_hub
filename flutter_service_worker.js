01af31825bf0fd5e58111346ad63b021fbba5f3e
Vitalie Andries <35801489+Vitalie-Andries@users.noreply.github.com>
2022-04-18 14:24:46 +0300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6b57ec25c6cf60ec46a13b03ddb28213",
-"index.html": "f8b502f6f7b8c473fd26ddb53f9d022b",
-"/": "f8b502f6f7b8c473fd26ddb53f9d022b",
-"main.dart.js": "c9b97b419e264dd6d5c779b49618fd74",
+"index.html": "03a8a8b485e9b9b513577cd123cc7b04",
+"/": "03a8a8b485e9b9b513577cd123cc7b04",
+"main.dart.js": "f152dbcbcc661143ae3a30ba77ad13b4",
 "favicon.png": "06f7ffb5e40e910067f51e2bcd3a9d95",
 "icons/Icon-192.png": "20efc95f17bf9893bed9be44e3b98bcf",
 "icons/Icon-maskable-192.png": "575eceea9ff624c4a5cf51d11c994678",
@@ -38,11 +38,7 @@ const RESOURCES = {
 "assets/assets/fonts/Montserrat-SemiBold.ttf": "c641dbee1d75892e4d88bdc31560c91b",
 "assets/assets/fonts/Montserrat-Regular.ttf": "ee6539921d713482b8ccd4d0d23961bb",
 "assets/assets/fonts/Inter-SemiBold.ttf": "ec60b23f3405050f546f4765a9e90fec",
-"assets/assets/gif/intro.gif": "b330acd8ed55b93bad4d465fab1bb5aa",
-"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
-"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
+"assets/assets/gif/intro.gif": "b330acd8ed55b93bad4d465fab1bb5aa"
 };
 
 // The application shell files that are downloaded before a service worker can
